4167e18c942b6859e7c3580fe52c3ea0f4334840
Samir Rashid <samirsgotmail@gmail.com>
2024-01-10 13:46:02 -0800

    nix processing

--- shell.nix
+++ shell.nix
@@ -14,19 +14,21 @@
 #  $ nix-shell
 #
 
-{ pkgs ? import <nixpkgs> {}, withUnfreePkgs ? false, useNightlyToolchain ? true }:
+{ pkgs ? import <nixpkgs> { }, withUnfreePkgs ? false }:
 
 with builtins;
 let
   inherit (pkgs) stdenv lib;
 
   # Tockloader v1.11.0
-  tockloader = import (pkgs.fetchFromGitHub {
-    owner = "tock";
-    repo = "tockloader";
-    rev = "v1.11.0";
-    sha256 = "sha256-bPEfpfOZOjOiazqRgn1cnqe4ohLPvocuENKoZx/Qw80=";
-  }) { inherit pkgs withUnfreePkgs; };
+  tockloader = import
+    (pkgs.fetchFromGitHub {
+      owner = "tock";
+      repo = "tockloader";
+      rev = "v1.11.0";
+      sha256 = "sha256-bPEfpfOZOjOiazqRgn1cnqe4ohLPvocuENKoZx/Qw80=";
+    })
+    { inherit pkgs withUnfreePkgs; };
 
   # Rust toolchain overlay
   rust_overlay = import "${pkgs.fetchFromGitHub {
@@ -40,55 +42,68 @@ let
 
   # Use nightly development toolchain by default because Miri is not supported
   # by the MSRV (Minimum Supported Rust Version) toolchain.
-  rustBuild = if useNightlyToolchain then (
-    nixpkgs.fenix.fromToolchainFile { file = ./nightly/rust-toolchain.toml; }
-  ) else (
-    nixpkgs.fenix.fromToolchainFile { file = ./rust-toolchain.toml; }
-  );
+  rustBuild =
+    foldl'
+      combine
+      ((nixpkgs.fenix.fromToolchainName {
+        name = (lib.importTOML ./nightly/rust-toolchain.toml).toolchain.channel;
+        sha256 = "sha256-R/ONZzJaWQr0pl5RoXFIbnxIE3m6oJWy/rr2W0wXQHQ=";
+      }).withComponents
+        ((lib.importTOML ./nightly/rust-toolchain.toml).toolchain.components ++
+          (lib.importTOML ./rust-toolchain.toml).toolchain.components))
+      (
+        (lib.importTOML ./rust-toolchain.toml).toolchain.targets
+      )
+  ;
+  # nixpkgs.fenix.toolchainOf {
+  #   # file = ./nightly/rust-toolchain.toml; 
+  #   channel = (lib.importTOML ./nightly/rust-toolchain.toml).toolchain.channel;
+  # }.withComponents;
+  # temp.withComponents = (lib.importTOML ./nightly/rust-toolchain.toml).toolchain.components;
 
 in
-  pkgs.mkShell {
-    name = "tock-dev";
+pkgs.mkShell {
+  name = "tock-dev";
 
-    buildInputs = with pkgs; [
-      # --- Toolchains ---
-      rustBuild
-      openocd
+  buildInputs = with pkgs; [
+    # --- Toolchains ---
+    rustBuild
+    openocd
 
-      # --- Convenience and support packages ---
-      python3Full
-      tockloader
+    # --- Convenience and support packages ---
+    python3Full
+    tockloader
 
-      # Required for tools/print_tock_memory_usage.py
-      python3Packages.cxxfilt
+    # Required for tools/print_tock_memory_usage.py
+    python3Packages.cxxfilt
 
 
-      # --- CI support packages ---
-      qemu
+    # --- CI support packages ---
+    qemu
 
-      # --- Flashing tools ---
-      # If your board requires J-Link to flash and you are on NixOS,
-      # add these lines to your system wide configuration.
+    # --- Flashing tools ---
+    # If your board requires J-Link to flash and you are on NixOS,
+    # add these lines to your system wide configuration.
 
-      # Enable udev rules from segger-jlink package
-      # services.udev.packages = [
-      #     pkgs.segger-jlink
-      # ];
+    # Enable udev rules from segger-jlink package
+    # services.udev.packages = [
+    #     pkgs.segger-jlink
+    # ];
 
-      # Add "segger-jlink" to your system packages and accept the EULA:
-      # nixpkgs.config.segger-jlink.acceptLicense = true;
-    ];
+    # Add "segger-jlink" to your system packages and accept the EULA:
+    # nixpkgs.config.segger-jlink.acceptLicense = true;
+  ];
 
-    LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+  LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
 
-    # Instruct the Tock gnumake-based build system to not check for rustup and
-    # assume all requirend tools are installed and available in the $PATH
-    NO_RUSTUP = "1";
+  # Instruct the Tock gnumake-based build system to not check for rustup and
+  # assume all requirend tools are installed and available in the $PATH
+  NO_RUSTUP = "1";
 
-    # The defaults "objcopy" and "objdump" are wrong (stem from the standard
-    # environment for x86), use "llvm-obj{copy,dump}" as defined in the makefile
-    shellHook = ''
-      unset OBJCOPY
-      unset OBJDUMP
-    '';
-  }
+  # The defaults "objcopy" and "objdump" are wrong (stem from the standard
+  # environment for x86), use "llvm-obj{copy,dump}" as defined in the makefile
+  shellHook = ''
+    unset OBJCOPY
+    unset OBJDUMP
+  '';
+}
